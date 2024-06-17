006e1201b45facadae5bcbde33a291305aa8db36
nguyenkhangph <pnkhang2703@gmail.com>
2024-06-18 04:04:29 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  plugins: [react()],
   base: '/MondiansTAP/', // Adjust if your GitHub Pages path is different
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: path.resolve(__dirname, 'frontend', 'public', 'index.html')  // Correct path using path.resolve
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'frontend', 'src')
     }
   }
 });
